896e84b7d39c02bf870a9b7877dcdf9c5af6e628
fionna <fionnatong@gmail.com>
2020-09-01 11:09:27 +0800

    feat: added css for list of todos

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,36 @@
 import React, { useContext } from "react";
+import { Button } from "antd";
 import { TodoContext } from "../context/TodoContextProvider";
 
-//TODO:
+const TodoTask = (props) => (
+  <div className="todo-task">
+    <div className="todo-task__name">{props.todo}</div>
+    <Button
+      type="primary"
+      shape="round"
+      className="todo-task__button"
+      data-cy="todo-task__button-update"
+    >
+      Update
+    </Button>
+    <Button
+      type="primary"
+      shape="round"
+      className="todo-task__button"
+      data-cy="todo-task__button-delete"
+    >
+      Delete
+    </Button>
+  </div>
+);
+
 export const TodoList = () => {
   const { state } = useContext(TodoContext);
-  console.log(state);
-  return <div data-cy="todo-list">{state.todos.join()}</div>;
+  return (
+    <div className="todo-list" data-cy="todo-list">
+      {state.todos.map((todo, index) => (
+        <TodoTask todo={todo} key={index} />
+      ))}
+    </div>
+  );
 };
