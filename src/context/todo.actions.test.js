53b19e80e4a0c9c4074ed48ea66b693cc0e2b8f4
Jian Sheng <jiansheng.govtech@outlook.com>
2020-09-03 09:09:24 +0800

    Add tests for SET_TODOS

--- src/context/todo.actions.test.js
+++ src/context/todo.actions.test.js
@@ -1,20 +1,35 @@
 const {
+  setTodos,
   addTodo,
   updateTodo,
   deleteTodo,
+  SET_TODOS,
   ADD_TODO,
   UPDATE_TODO,
   DELETE_TODO,
 } = require("./todo.actions");
 
 describe("todo.actions", () => {
+  describe("setTodos", () => {
+    it("should dispatch action with type SET_TODOS", () => {
+      expect(setTodos({ "0": { description: "todo!" } })).toEqual({
+        type: SET_TODOS,
+        data: {
+          "0": {
+            description: "todo!",
+          },
+        },
+      });
+    });
+  });
+
   describe("addTodo", () => {
     it("should dispatch action with type ADD_TODO", () => {
       expect(addTodo(0, "new todo!")).toEqual({
         type: ADD_TODO,
         data: {
           id: 0,
-          description: "new todo!"
+          description: "new todo!",
         },
       });
     });
--- src/context/todo.reducer.test.js
+++ src/context/todo.reducer.test.js
@@ -1,7 +1,19 @@
-const { ADD_TODO, UPDATE_TODO, DELETE_TODO } = require("./todo.actions");
+const { SET_TODOS, ADD_TODO, UPDATE_TODO, DELETE_TODO } = require("./todo.actions");
 const { todoReducer } = require("./todo.reducer");
 
 describe("todo.reducer", () => {
+  it("should add data to state.todos for type SET_TODOS", () => {
+    const mockState = { todos: {} };
+    const mockAction = { type: SET_TODOS, data: { "0": { description: "new todo!" } } };
+    expect(todoReducer(mockState, mockAction)).toEqual({
+      todos: {
+        "0": {
+          description: "new todo!",
+        },
+      },
+    });
+  });
+
   it("should add data to state.todos for type ADD_TODO", () => {
     const mockState = { todos: {} };
     const mockAction = { type: ADD_TODO, data: { id: 0, description: "new todo!" } };
@@ -20,7 +32,7 @@ describe("todo.reducer", () => {
     expect(todoReducer(mockState, mockAction)).toEqual(mockState);
   });
 
-  fdescribe("UPDATE_TODO", () => {
+  describe("UPDATE_TODO", () => {
     it("should update data of specific id in state.todos", () => {
       const mockState = {
         todos: {
